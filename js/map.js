fdf997253b90e69a33b5119440585d3b8222bfd2
alejandrobarriuso <alejandro.barriuso@cchs.csic.es>
2018-06-06 17:55:28 +0200

    Cambio barra superior + mapa y primeros controles

--- js/map.js
+++ js/map.js
@@ -1,14 +1,51 @@
+/* MAPA */
 var layer = new ol.layer.Tile({
   source: new ol.source.OSM()
 });
 
+/* Centro (Lon, Lat) y zoom inicial del mapa */
+var LonLat_centro = [-3, 39];
+var Zoom_inicial = 6;
+
+/* Controles del mapa */
+var scaleLineControl = new ol.control.ScaleLine();
+
+var mousePositionControl = new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(4),
+        projection: 'EPSG:4326',
+        // comment the following two lines to have the mouse position
+        // be placed within the map.
+        className: 'custom-mouse-position',
+        target: document.getElementById('mouse-position'),
+        undefinedHTML: '&nbsp;'
+      });
+
+/* Creación del mapa y de la vista */
 var map = new ol.Map({
   layers: [layer],
   target: 'map',
   view: new ol.View({
-    center: [0, 0],
-    zoom: 2
-  })
+    projection: 'EPSG:3857',
+    center: ol.proj.fromLonLat(LonLat_centro),
+    zoom: Zoom_inicial,
+    minZoom: 2,
+    maxZoom: 28,
+    extent: [-20026376.39, -20048966.10, 20026376.39, 20048966.10]
+  }),
+  controls: ol.control.defaults({
+    attributionOptions: {
+      collapsible: false
+    }
+  }).extend([
+    scaleLineControl, mousePositionControl
+  ]),
 });
 
-var pos = ol.proj.fromLonLat([16.3725, 48.208889]);
+
+
+
+function mostrar_div_nodos() {
+    div = document.getElementById('div_nodos');
+    div.style.display = '';
+    console.log("hola");
+}
