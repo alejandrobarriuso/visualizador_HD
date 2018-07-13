d75e9b319a7ed59671f5180126d20c447abd3236
alejandrobarriuso <alejandro.barriuso@cchs.csic.es>
2018-07-13 13:29:14 +0200

    Menú de nodos mejorado, con array externo de nodos

---
+++ js/menuSuperior/arrayNodos.js
@@ -0,0 +1,40 @@
+//Array de nodos:
+var arrayNodos = [
+  {
+    "id": "idearq",
+    "nombre": "IDEARQ",
+    "color_fondo": "#FFB299",
+    "color_letra_borde": "#DF3A2C",
+    "url": "http://www.idearqueologia.org/"
+  }, {
+    "id": "sig_mayores",
+    "nombre": "SIG MAYORES",
+    "color_fondo": "#00ADAD",
+    "color_letra_borde": "#DF3A2C",
+    "url": "http://sigmayores.csic.es/visor/visor.html"
+  }, {
+    "id": "prueba1",
+    "nombre": "PRUEBA1",
+    "color_fondo": "#F3AB00",
+    "color_letra_borde": "#DF3A2C",
+    "url": ""
+  }, {
+    "id": "prueba2",
+    "nombre": "PRUEBA2",
+    "color_fondo": "#008A47",
+    "color_letra_borde": "#DF3A2C",
+    "url": "http://www.idearqueologia.org/"
+  }, {
+    "id": "prueba3",
+    "nombre": "PRUEBA3",
+    "color_fondo": "#ED7500",
+    "color_letra_borde": "#DF3A2C",
+    "url": "http://sigmayores.csic.es/visor/visor.html"
+  }, {
+    "id": "prueba4",
+    "nombre": "PRUEBA4",
+    "color_fondo": "#7D3280",
+    "color_letra_borde": "#DF3A2C",
+    "url": ""
+  }
+];
---
+++ js/menuSuperior/menuNodos.js
@@ -0,0 +1,32 @@
+//FUNCIÓN CargarTematicas(numMaxTematicas,idioma)
+/*
+ENTRADAS:
+  numMaxTematicas: número máximo de temáticas a mostrar.
+  idioma: variable que habrá cogido su valor al clickar sobre los botones de selección de idioma (archivo ponerIdioma.js). Por defecto: 'es'.
+FUNCIONALIDAD:
+  Crea un cuadrado por cada temática y un submenú asociado, que sólo se mostrará cuando se ejecute la función AparecerSubmenuTematicaNumi(idTematica,colorFondo,colorLetraBorde),
+  a la que se llama desde los propios elementos creados aquí.
+*/
+
+function CargarNodos(numMaxNodos){
+  console.log(numMaxNodos);
+  console.log(arrayNodos.length);
+  //Crear un cuadrado por cada nodo:
+  for (var i=0; i<Math.min(numMaxNodos,arrayNodos.length); i++){
+    var nodoNumi = document.createElement("a");
+    nodoNumi.innerHTML = '<h4>' + arrayNodos[i].nombre.toUpperCase() + '</h4>';
+
+    var idNodoNumi = "nodo" + arrayNodos[i].id;
+    nodoNumi.setAttribute("id",idNodoNumi);
+    nodoNumi.setAttribute("class","card m-0 p-0");
+    nodoNumi.setAttribute("style","height:9em;width:10em;border-radius:0;border-width:0;display:flex;justify-content:center;align-items:center;text-decoration:none;" +
+      ";background-color:" + arrayNodos[i].color_fondo +
+      ";color:#f2f2f2"
+    );
+    console.log(nodoNumi);
+    //Añadir el objeto Nodo al menú superior:
+    nodoNumi.setAttribute("href",arrayNodos[i].url);
+    nodoNumi.setAttribute("target","_blank");
+    document.getElementById("espNodos").appendChild(nodoNumi);
+  };
+}
